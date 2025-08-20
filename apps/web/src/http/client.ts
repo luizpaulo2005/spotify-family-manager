c48f35c91bf05399aa8fdae8c5a309a05fe16cd3
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-08-19 21:46:45 -0300

    feat: setup api client

---
+++ apps/web/src/http/client.ts
@@ -0,0 +1,27 @@
+import { env } from '@spotify-family-manager/env'
+import { type CookiesFn, getCookie } from 'cookies-next'
+import ky from 'ky'
+
+const api = ky.create({
+  prefixUrl: env.NEXT_PUBLIC_API_URL,
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let cookieStore: CookiesFn | undefined
+
+        if (typeof window === 'undefined') {
+          const { cookies: serverCookies } = await import('next/headers')
+
+          cookieStore = serverCookies
+        }
+        const token = await getCookie('token', { cookies: cookieStore })
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
+    ],
+  },
+})
+
+export { api }
