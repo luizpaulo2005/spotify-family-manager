ba301b2120a31bf7ae9bb640816b8d5d30728411
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-09-08 13:49:56 -0300

    feat: update package.json and tsup.config.ts for improved script execution and module format

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src'],
   splitting: false,
+  format: ['esm'],
   sourcemap: true,
   clean: true,
   noExternal: ['@spotify-family-manager/env'],
